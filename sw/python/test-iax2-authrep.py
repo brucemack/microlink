6a367f948811c6365417c6bf3a0a18232134ce98
Bruce MacKinnon <bruce@mackinnon.com>
2025-10-08 10:26:17 -0400

    Adding test case for AllStarLink

---
+++ sw/python/test-iax2-authrep.py
@@ -0,0 +1,63 @@
+"""
+This is a demonstration of the challenge validation process 
+used by AllStarLink (IAX2).
+
+Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+"""
+import base64
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.exceptions import InvalidSignature
+
+"""
+From RFC-5456, Section 8.6.16:
+
+The purpose of the RSA RESULT information element is to offer an RSA
+response to an authentication CHALLENGE.  It carries the UTF-8-
+encoded challenge result.  The result is computed as follows: first,
+compute the SHA1 digest [RFC3174] of the challenge string and second,
+RSA sign the SHA1 digest using the private RSA key as specified in
+PKCS #1 v2.0 [PKCS].  The RSA keys are stored locally.
+
+Upon receiving an RSA RESULT information element, its value must be
+verified with the sender's public key to match the SHA1 digest
+[RFC3174] of the challenge string.
+"""
+
+# The RSA public key is provided in the ASL3 installation. On the Pi
+# appliance distribution it is located at:
+#   /usr/share/asterisk/keys/allstar.pub
+# The public key is in PEM format:
+public_key_pem = "-----BEGIN PUBLIC KEY-----\n\
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCu3h0BZQQ+s5kNM64gKxZ5PCpQ\n\
+9BVzhl+PWVYXbEtozlJVVs1BHpw90GsgScRoHh4E76JuDYjEdCTuAwg1YkHdrPfm\n\
+BUjdw8Vh6wPFmf3ozR6iDFcps4/+RkCUb+uc9v0BqZIzyIdpFC6dZnJuG5Prp7gJ\n\
+hUaYIFwQxTB3v1h+1QIDAQAB\n\
+-----END PUBLIC KEY-----\n"
+public_key = serialization.load_pem_public_key(public_key_pem.encode("utf-8"))
+
+# The RSA challenge string is a 9-digit number created randomly by 
+# the server creating the AUTHREQ message. Here is an example that 
+# was captured from the network during an actual connection:
+rsa_challenge = "570639908"
+
+# The RSA signature is created by the client attempting to connect.
+# This is created using the client's private key. The signature
+# is sent back in the AUTHREP message. Here is an example that
+# was captured from the network during an actual connection:
+rsa_challenge_result_base64 = "ZanWw1+Wx5TWWX6g4890bmnflMgk8ZyyRdjINenNmzq3eYWfPMpcfMFIrHfX0gxOzGeNflcbOqr1m6GMnCoE92h+fMlIEZceUuCZXh+GZ4ywiy3RJluvE/Cj/vkh5Af38jb5PjT2dJB/HMZ8mSZ7qDQgcjjotNRmWVGhAMte9Nc="
+
+# The signature in the AUTHREP message is actually a base-64 encoding 
+# of the signature:
+rsa_challenge_result = base64.b64decode(rsa_challenge_result_base64.encode("utf-8"))
+
+# Here is where the actual validation happens:
+public_key.verify(rsa_challenge_result,
+    rsa_challenge.encode("utf-8"), 
+    padding.PKCS1v15(), 
+    hashes.SHA1())
+
+# If we get here the validation is good, otherwise an exception is raised
+print("AUTHREP signature validated, all good!")
